1155aad3e11101278990ddd68781a74692c1da86
mariaStratigi <47947413+mariaStratigi@users.noreply.github.com>
2022-08-04 17:47:53 +0300

    Add files via upload

---
+++ SeqEnv.py
@@ -0,0 +1,478 @@
+from tensorforce.environments import Environment
+from tensorforce.agents import Agent
+import numpy as np
+
+
+###-----------------------------------------------------------------------------
+### Environment definition
+class SQUIRRELEnvironment(Environment):
+    """This class defines a simple thermostat environment.  It is a room with
+    a heater, and when the heater is on, the room temperature will approach
+    the max heater temperature (usually 1.0), and when off, the room will
+    decay to a temperature of 0.0.  The exponential constant that determines
+    how fast it approaches these temperatures over timesteps is tau.
+    """
+    def __init__(self):
+        ## Some initializations.  Will eventually parameterize this in the constructor.
+        #self.tau = 3.0
+        #self.current_temp = np.random.random(size=(1,))
+        #self.en = []
+        #i = 0
+        #for i in range(15):
+        #    self.en[i] = 0.0
+
+        self.en = []
+        self.en.append(0.0)
+        self.en.append(0.0)
+        self.en.append(0.0)
+        self.en.append(0.0)
+        self.en.append(0.0)
+
+        self.iterSat = []
+        self.iterSat.append(0.0)
+        self.iterSat.append(0.0)
+        self.iterSat.append(0.0)
+        self.iterSat.append(0.0)
+        self.iterSat.append(0.0)
+        self.flagEmpty = False
+        #self.rec = []
+        super().__init__()
+
+
+    def states(self):
+        return dict(type='float', shape=(5,), min_value=0.0, max_value=5.0)
+
+
+    def actions(self):
+        """Action 0 means no heater, temperature approaches 0.0.  Action 1 means
+        the heater is on and the room temperature approaches 1.0.
+        """
+        return dict(type='int', num_values=6)
+
+
+    # Optional, should only be defined if environment has a natural maximum
+    # episode length
+    def max_episode_timesteps(self):
+        return super().max_episode_timesteps()
+
+
+    # Optional
+    def close(self):
+        super().close()
+
+
+    def reset(self):
+        """Reset state.
+        """
+        # state = np.random.random(size=(1,))
+        self.timestep = 0
+        #self.current_temp = np.random.random(size=(1,))
+        self.en = []
+        self.iterSat = []
+        self.flagEmpty = False
+        for i in range(5):
+            self.en.append(0.0)
+            self.iterSat.append(0.0)
+        #return self.current_temp
+        return self.en
+
+
+
+
+
+    def getLeastSatUser(self):
+        min = 1000.0
+        ind = -1
+        i = 0
+        for x in self.en:
+            if x < min:
+                min = x
+                ind = i
+            i = i + 1
+        return ind + 1
+
+
+
+    def getAverage(self,item):
+        sum = 0
+        i = 0
+        for u in self.users:
+            i = i + 1
+            rec = self.users[u]
+            if item in rec.keys():
+                sum = sum + float(rec[item])
+        return sum / i
+
+
+
+    def getLeastScore(self,item,ind):
+        i = 0
+        sc = 0
+        for u in self.users:
+            if i != ind:
+                i = i + 1
+                continue
+            rec = self.users[u]
+            if item in rec.keys():
+                sc = float(rec[item])
+            break
+        return sc
+
+
+
+
+    def getTopK(self,scores,k):
+        sc = {k: v for k, v in sorted(scores.items(), key=lambda item: item[1], reverse = True)}
+        i = 0
+        self.recs = []
+        for it in sc.keys():
+            self.recs.append(it)
+            i = i + 1
+            if i == k:
+                break
+        return self.recs
+
+
+
+    def getTopKUser(self,scores, k):
+        sc = {k: v for k, v in sorted(scores.items(), key=lambda item: item[1], reverse = True)}
+        i = 0
+        sum = 0
+        for x in sc.keys():
+            i = i + 1
+            sum = sum + float(sc[x])
+            if i == k:
+                break
+        return sum
+
+
+
+    def getRecRel(self, scores):
+        sum = 0
+        for x in self.recs:
+            if x in scores.keys():
+                sum = sum + float(scores[x])
+        return sum
+
+    def getRecRelPartial(self, scores, single):
+        sum = 0
+        for x in scores:
+            if x in single.keys():
+                sum = sum + float(single[x])
+        return sum
+
+
+
+
+
+    """
+    Updates the satisfaction of the members based on the top 10 recommendations
+    """
+    def getSatisfactions(self, k):
+        allS = []
+        i=0
+        for y in self.users:
+            rec = self.users[y]
+            arith = self.getRecRel(rec)
+            para = self.getTopKUser(rec,k)
+            sat = 0
+            if para == 0:
+                self.flagEmpty = True
+                for x in rec.keys():
+                    print(str(x) + "\t" + str(rec[x]))
+            else:
+                sat = arith / para
+            satNew = self.en[i]+((sat- self.en[i])/self.timestep)
+            self.en[i] = satNew
+            allS.append(sat)
+            i = i +1
+        return allS
+
+
+
+    def setUsers(self,users):
+        self.users = users
+        return
+    """
+    Returns a list of all unique items in all individual list of the Users
+    """
+    def setCandidates(self,usr):
+        cand = []
+        for u in usr:
+            rec = usr[u]
+            for r in rec:
+                if r in cand:
+                    continue
+                cand.append(r)
+        self.candidates = cand
+        return
+
+
+    """
+    def computeReward(self,sats): #--->Fscore<--- between members _overall satisfaction_
+        max = -100.0
+        min = 100.0
+        sum = 0
+        for x in self.en:
+            if x > max:
+                max = x
+            if x < min:
+                min = x
+            sum = sum + x
+        sum = sum / len(self.en)
+        maxMin = max - min
+        maxMin = 1 - maxMin
+        fScore = 2*((sum*maxMin)/(sum + maxMin))
+        #print(str(fScore))
+        return fScore
+    """
+
+
+    def computeReward(self, sats): #--->average<--- between members _iteration satisfaction_
+        i = 0
+        sum = 0
+        for s in self.en:
+            sum = sum + s
+            i = i + 1
+        reward = sum / i
+        return reward
+
+
+    """
+    def computeReward(self, sats): #--->Variance<--- between members _iteration satisfaction_
+        aa = np.array(list(sats))
+        reward = np.var(aa)
+        return 1-reward
+    """
+    #Calculates the alpha for the SDAA method
+    def calcAlpha(self):
+        max = -100.0
+        min = 100.0
+        for d in self.en:
+            if d > max:
+                max = d
+            if d < min:
+                min = d
+        a = max - min
+        return a
+    """
+    #Average aggregation method
+    """
+    def aggregateAvg(self):
+        scores = {}
+        i = 0
+        for sc in self.candidates:
+            avg = self.getAverage(sc)
+            scores[sc] = avg
+        return scores
+    """
+    #SDAA aggregation method
+    """
+    def aggregateSDAA(self):
+        leastSat = self.getLeastSatUser()
+        #scores = np.zeros(len(self.candidate))
+        scores = {}
+        i = 0
+        a = self.calcAlpha()
+        for sc in self.candidates:
+            avg = self.getAverage(sc)
+            lm = self.getLeastScore(sc,leastSat)
+
+            gSc = (1-a)*avg
+            gSc = gSc + a*lm
+            scores[sc] = gSc
+            i = i + 1
+        return scores
+    """
+    SIAA aggregation method
+    """
+    def calcWeights(self):
+        w = []
+        max = -100.0
+        for s in self.iterSat:
+            if s > max:
+                max = s
+        for i in range(5):
+            tmp = 0.8*(1-self.en[i]) + 0.2*(max-self.iterSat[i])
+            w.append(tmp)
+        return w
+    """
+    #SIAA aggregation method
+    """
+    def aggregateSIAA(self):
+        scores = {}
+        if self.timestep == 0:
+            scores = self.aggregateAvg()
+        else:
+            weights = self.calcWeights()
+            for sc in self.candidates:
+                sum = 0
+                i = 0
+                for u in self.users:
+                    rec = self.users[u]
+                    if sc in rec.keys():
+                        sum = sum + weights[i] * float(rec[sc])
+                    i = i + 1
+                sum = sum / i
+                scores[sc] = sum
+        return scores
+    """
+    Calculates partial satisfaction scores for the Avg+ aggregation method
+    """
+    def calcPartialSatScore(self,recGroup):
+        allS = []
+        i=0
+        for y in self.users:
+            rec = self.users[y]
+            arith = self.getRecRelPartial(recGroup,rec)
+            para = self.getTopKUser(rec,len(recGroup))
+            sat = 0
+            if para == 0:
+                #print(str(y) + " size of recs: " + str(len(rec)) + " at iteration: " + str(self.timestep))
+                self.flagEmpty = True
+                sat = 0
+            else:
+                sat = arith / para
+            allS.append(sat)
+            #print(sat)
+            i = i +1
+        return allS
+
+    def calcGroupDis(self,sats):
+        max = -100.0
+        min = 100
+        for s in sats:
+            if s > max:
+                max = s
+            if s < min:
+                min = s
+        dis = max - min
+        return dis
+
+    def aggregateAvgPlus(self):
+        avgScores = self.aggregateAvg()
+        cand = self.getTopK(avgScores,50)
+        scores = {}
+        while len(scores) < 5:  #here
+            tmp = {}
+            min = 100.0
+            id = ""
+            for sc in scores.keys():
+                tmp[sc] = scores[sc]
+            for sc in cand:
+                if sc in scores.keys():
+                    continue
+                tmp[sc] = avgScores[sc]
+                sats = self.calcPartialSatScore(tmp)
+                dis = self.calcGroupDis(sats)
+                if dis < min:
+                    min = dis
+                    id = sc
+                del tmp[sc]
+            scores[id] = min
+        return scores
+
+
+    def aggregatePareto(self):
+        scores = {}
+
+        while len(scores) < 5: #here
+            tmp = {}
+            max = -100.0
+            id = ""
+            for sc in scores.keys():
+                tmp[sc] = scores[sc]
+            for sc in self.candidates:
+                if sc in scores.keys():
+                    continue
+                tmp[sc] = 0.0
+                sats = self.calcPartialSatScore(tmp)
+                sw = 0
+                for s in sats:
+                    sw = sw + s
+                sw = sw / len(sats)
+                f = np.var(sats)
+                f = 1-f
+                score = 0.8*sw + 0.2*f
+                if score > max:
+                    max = score
+                    id = sc
+                del tmp[sc]
+            scores[id] = max
+        return scores
+
+    def aggregateSihem(self):
+        scores = {}
+
+        for sc in self.candidates:
+            a = 0
+            i = 0
+            dis = 0
+            for y in self.users:
+                rec = self.users[y]
+                o1 = 0
+                if sc in rec.keys():
+                    a = a + float(rec[sc])
+                    o1 = float(rec[sc])
+                j = 0
+                for u in self.users:
+                    if i == j:
+                        continue
+                    j = j + 1
+                    rec2 = self.users[u]
+                    o2 = 0
+                    if sc in rec2.keys():
+                        o2 = float(rec2[sc])
+                    dis = dis + abs(o1-o2)
+                i = i + 1
+            dis = (2*dis)/(len(self.users)*(len(self.users)-1))
+            a = a / len(self.users)
+            score = 0.2*a + 0.8*dis
+            scores[sc] = score
+        return scores
+
+    def execute(self,actions):
+        ## Check the action is either 0 or 1 -- heater on or off.
+        #assert actions == 0 or actions == 1
+
+        ## Increment timestamp
+        self.timestep += 1
+
+        ## Update the current_temp
+        #self.current_temp = self.response(actions)
+
+        self.setCandidates(self.users)
+
+
+
+        if actions == 0:
+            m = self.aggregateAvg()
+        elif actions == 1:
+            m = self.aggregateSDAA()
+        elif actions == 2:
+            m = self.aggregateSIAA()
+        elif actions == 3:
+            m = self.aggregateAvgPlus()
+        elif actions == 4:
+            m = self.aggregatePareto()
+        elif actions == 5:
+            m = self.aggregateSihem()
+        else:
+            print("ERROR---ERROR")
+
+
+        recs = self.getTopK(m,10)    #here
+        satS = self.getSatisfactions(10) #here
+        self.iterSat = satS
+        ## Compute the reward
+        reward = self.computeReward(satS)
+
+        ## The only way to go terminal is to exceed max_episode_timestamp.
+        ## terminal == False means episode is not done
+        ## terminal == True means it is done.
+        if self.timestep == 15:
+            terminal = True
+        else:
+            terminal = False
+
+        return self.en, terminal, reward
---
+++ seqTest.py
@@ -0,0 +1,421 @@
+import numpy as np
+import scipy.sparse as sp
+import math
+from SeqEnv import SQUIRRELEnvironment
+#from tensorforce.environments import Environment
+from tensorforce import Agent, Environment
+from sklearn.preprocessing import MinMaxScaler
+
+
+
+def getTopK(k, id, items, lfm):
+    itm = np.array(list(items))
+    res = lfm.predict(id,itm)
+    res = np.sort(res)[::-1]
+    top = []
+    for i in range(k):
+        top.append(res[i])
+    return top
+"""
+#file: the name of the file containing the groups
+#path: change as needed: the full path of the folder containing the file of the groups
+#return: A list of Strings of the form: "groupId    groupSim" of all groups in the file
+"""
+def readGroups(file,path='C:/Users/jarim/Documents/work/rnn/data/'):
+    groupIds = []
+    with open(path+file) as f:
+        for line in f:
+            groupIds.append(line)
+    return groupIds
+"""
+#groupId: A string of the form "groupId '\t' groupSim"
+#The groupId is a concatenation of all the ids of the members
+#return: Alist with the ids of the group members
+"""
+def getMembers(groupId):
+    members = []
+    (gr,sim) = groupId.split("\t")
+    (m1,m2,m3,m4,m5) = gr.split("_")
+    members.append(int(m1))
+    members.append(int(m2))
+    members.append(int(m3))
+    members.append(int(m4))
+    members.append(int(m5))
+    #if the group size is larger than 5 uncommend as needed
+    #members.append(int(m6))
+    #members.append(int(m7))
+    #members.append(int(m8))
+    #members.append(int(m9))
+    return members
+
+
+def calcStats(sats):
+    sum = 0.0
+    max = -1000.0
+    min = 1000.0
+
+    for k in sats:
+        sum = sum + k
+        if k < min:
+            min = k
+        if k > max:
+            max = k
+    maxMin = max - min
+    sum = sum / len(sats)
+    fScore = 2*((sum*maxMin)/(sum + maxMin))
+    return (sum,maxMin,fScore)
+"""
+#Read the already calculated single recommendations for all group members and all rounds of recommendations
+#returns: A dictionary with key: round of recommendations  value: a dictionary U
+#dictionary U keys: user id value: dictionary R
+#dictionary R keys: itemId value: score
+"""
+def getPredictions(file):
+    groupInf = {}
+    groupRec = {}
+    rec = {}
+    iter = 0
+    count = 0
+    with open(file+".txt") as f:
+        for l in f:
+            if "Iteration" in l:
+                groupRec = {}
+                count = 0
+                continue
+            count = count + 1
+            rec = {}
+            firstSplit = l.split("[")
+            id = firstSplit[0]
+            allRecs = firstSplit[1].replace("]","")
+            allRecs = allRecs.replace("\n","")
+            secondSplit = allRecs.split(",")
+            for r in secondSplit:
+                itm = r.split(":")
+                rec[itm[0]] = itm[1]
+            groupRec[id] = rec
+            if count == 5:
+                groupInf[iter] = groupRec
+                iter = iter + 1
+    return groupInf
+
+
+def updateRecs(group, iter, recMovies):
+    gr = group[iter]
+    users = {}
+    i = 0
+    for u in gr:
+        recs = gr[u]
+        userRecs = {}
+        for m in recs:
+            if m in recMovies:
+                continue
+            else:
+                userRecs[m] = recs[m]
+        users[u] = userRecs
+    return users
+
+def computeIDCG(n):
+    idcg = 0
+    for i in range(n):
+        idcg = idcg + math.log(2) / math.log(i+2)
+    return idcg
+
+def computeNDCG(user,group):
+    dcg = 0.0
+    idcg = computeIDCG(len(group))
+    left_out = 0
+    size = len(user)
+    for i in range(size):
+        item = user[i]
+        if item in group:
+            rank = i + 1 - left_out
+            dcg = dcg + math.log(2) / math.log(rank + 1)
+    return dcg/idcg
+
+def getTopKItems(user,n):
+    i = 0
+    l = []
+    for x in user.keys():
+        if i == n:
+            break
+        l.append(x)
+        i = i + 1
+    return l
+
+def computeDFH(user,group):
+    i = 1
+    flag = False
+    for x in group:
+        if x in user:
+            flag = True
+            break
+        i = i + 1
+    if flag:
+        return 1 / math.log((i+1),2)
+    else:
+        return 0
+
+
+
+files = []
+
+
+#MovieLens
+
+files.append("ratingsStartingFull2003.csv")
+files.append("sem_2004_1.csv")
+files.append("sem_2004_2.csv")
+files.append("sem_2005_1.csv")
+files.append("sem_2005_2.csv")
+files.append("sem_2006_1.csv")
+files.append("sem_2006_2.csv")
+files.append("sem_2007_1.csv")
+files.append("sem_2007_2.csv")
+files.append("sem_2008_1.csv")
+files.append("sem_2008_2.csv")
+files.append("sem_2009_1.csv")
+files.append("sem_2009_2.csv")
+files.append("sem_2010_1.csv")
+files.append("sem_2010_2.csv")
+files.append("sem_2011_1.csv")
+
+
+#GoodReads - Amazon
+
+"""
+files.append("start.csv")
+files.append("chunks1.csv")
+files.append("chunks2.csv")
+files.append("chunks3.csv")
+files.append("chunks4.csv")
+files.append("chunks5.csv")
+files.append("chunks6.csv")
+files.append("chunks7.csv")
+files.append("chunks8.csv")
+files.append("chunks9.csv")
+files.append("chunks10.csv")
+files.append("chunks11.csv")
+files.append("chunks12.csv")
+files.append("chunks13.csv")
+files.append("chunks14.csv")
+"""
+
+#Read all the groups in the file
+groupsIds = readGroups('5DiffGroupsTrain.txt')
+
+
+print('Start training')
+environment = Environment.create(
+    environment=ThermostatEnvironment,
+    max_episode_timesteps=15)
+#agent = Agent.create(
+#    agent='tensorforce', environment=environment, update=2,
+#    optimizer=dict(optimizer='adam', learning_rate=3.2e-4),
+#    objective='policy_gradient', reward_estimation=dict(horizon=1)
+#)
+agent = Agent.create(agent='ppo.json', environment=environment)
+
+#****TRAINING******
+j =1
+#An array so as to calculate the number of times each action was selected
+#If more actions are added this needs to be increased accordingly
+
+actionsChoosenTrain = [0] * 6
+for gr in groupsIds:
+
+    grIn = gr.split("\t")
+    print('Training Group ' + " (" + str(j) + ")\t" + grIn[0] )
+    #groupInfo: the pre-calculated single recommendations for all group members and all rounds of recommendations
+    groupInfo = getPredictions("C:/Users/jarim/Documents/work/ephemeral/rnn/groupPredictions/allGroups/"+grIn[0].strip())
+
+    #group: a list of all group member
+    group = getMembers(gr)
+    states = environment.reset()
+    terminal = False
+    internals = agent.initial_internals()
+    i=0
+    recommentedMovies = []
+    while not terminal:
+        #groupRecs: the ratings of the group members without the items thar have been recommended
+        groupRecs = updateRecs(groupInfo, i, recommentedMovies)
+
+        actions = agent.act(states=states)
+        #get the action selected
+        actionsChoosenTrain[actions] = actionsChoosenTrain[actions] + 1
+        #send the group members info to the environment
+        environment.setUsers(groupRecs)
+        #run the agent
+        states, terminal, reward = environment.execute(actions=actions)
+        agent.observe(terminal=terminal, reward=reward)
+        #get the recommended items
+        rec = environment.recs
+
+        for k in rec:
+            recommentedMovies.append(k)
+
+
+        i = i + 1
+    j = j + 1
+
+print('training complete')
+
+
+#*****TESTING******
+
+### Initialize
+
+states = environment.reset()
+
+internals = agent.initial_internals()
+terminal = False
+
+### Run an episode
+
+groupsIds = readGroups('5DiffGroupsTest.txt')
+satO = {}
+maxMin = {}
+fScore = {}
+ndcg = {}
+dfh = {}
+j = 1
+actionsChoosen  = [0] * 6
+for groups in groupsIds:
+    grIn = groups.split("\t")
+    groupInfo = getPredictions("C:/Users/jarim/Documents/work/ephemeral/rnn/groupPredictions/allGroups/"+grIn[0].strip())
+    print('Training Group ' + " (" + str(j) + ")\t" + grIn[0] )
+    group = getMembers(groups)
+    i = 0
+    environment.reset()
+    states = environment.en
+    agent.reset()
+    internals = agent.initial_internals()
+    terminal = False
+    recommentedMovies = []
+    while not terminal:
+        print('\tIteration: ' + str(i))
+        groupRecs = updateRecs(groupInfo, i, recommentedMovies)
+
+        environment.setUsers(groupRecs)
+        actions, internals = agent.act(states=states, internals=internals, independent=True)
+        states, terminal, reward = environment.execute(actions=actions)
+        rec = environment.recs
+        actionsChoosen[actions] = actionsChoosen[actions] + 1
+        for k in rec:
+            recommentedMovies.append(k)
+        (sum,var,fScoreV) = calcStats(states)
+        ndcgV = 0.0
+        nn = 0
+        for gl in groupRecs:
+            glD = groupRecs[gl]
+            ul = getTopKItems(glD,5) #here
+            nd = computeNDCG(ul,rec)
+            ndcgV = ndcgV + nd
+            nn = nn + 1
+        ndcgV = ndcgV / nn
+
+        dfhV = 0.0
+        nn = 0
+        for gl in groupRecs:
+            glD = groupRecs[gl]
+            ul = glD.keys()
+            nd = computeDFH(ul,rec)
+            dfhV = dfhV + nd
+            nn = nn + 1
+        dfhV = dfhV / nn
+        if j == 1:
+            satOV = []
+            satOV.append(sum)
+            satO[str(i)] = satOV
+            mm = []
+            mm.append(var)
+            maxMin[str(i)] = mm
+            fs = []
+            fs.append(fScoreV)
+            fScore[str(i)] = fs
+            nn = []
+            nn.append(ndcgV)
+            ndcg[str(i)] = nn
+            dd = []
+            dd.append(dfhV)
+            dfh[str(i)] = dd
+        else:
+            satOV = satO[str(i)]
+            satOV.append(sum)
+            mm = maxMin[str(i)]
+            mm.append(var)
+            fs = fScore[str(i)]
+            fs.append(fScoreV)
+            nn = ndcg[str(i)]
+            nn.append(ndcgV)
+            dd = dfh[str(i)]
+            dd.append(dfhV)
+        i = i + 1
+    j = j + 1
+    fl = environment.flagEmpty
+    if fl:
+        print("Has empty values >>>>>>>>>>" + str(grIn[0]))
+with open('AllGroups_Goodreads_FScore.txt', 'w') as f:
+    f.write('Overall Satisfaction\n')
+
+    for i in range(15):
+        sum = 0
+        j = 0
+        tmp = satO[str(i)]
+        for y in tmp:
+            sum = sum + y
+            j = j + 1
+        sum = sum / j
+        f.write(str(sum) + '\n')
+
+    f.write('\nMaxMin\n')
+    for i in range(15):
+        sum = 0
+        j = 0
+        tmp = maxMin[str(i)]
+        for y in tmp:
+            sum = sum + y
+            j = j + 1
+        sum = sum / j
+        f.write(str(sum) + '\n')
+
+    f.write('\nNDCG\n')
+    for i in range(15):
+        sum = 0
+        j = 0
+        tmp = ndcg[str(i)]
+        for y in tmp:
+            sum = sum + y
+            j = j + 1
+        sum = sum / j
+        f.write(str(sum) + '\n')
+
+    f.write('\nDFH\n')
+    for i in range(15):
+        sum = 0
+        j = 0
+        tmp = dfh[str(i)]
+        for y in tmp:
+            sum = sum + y
+            j = j + 1
+        sum = sum / j
+        f.write(str(sum) + '\n')
+
+    f.write('\nFScore\n')
+    for i in range(15):
+        sum = 0
+        j = 0
+        tmp = fScore[str(i)]
+        for y in tmp:
+            sum = sum + y
+            j = j + 1
+        sum = sum / j
+        f.write(str(sum) + '\n')
+
+    f.close()
+    print(">>>>>Actions TRAIN<<<<<")
+    for act in actionsChoosenTrain:
+        print(str(act))
+
+    print("\n>>>>>Actions TEST<<<<<")
+    for x in actionsChoosen:
+        print(str(x))
